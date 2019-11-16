fdcd8e5ed3b873723955751f47d422ac70dd3039
tangweikun <819573105@qq.com>
2019-11-16 11:52:13 +0800

    :sparkles: add Step5

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import { Step1 } from "./components/Step1";
 import { Step2 } from "./components/Step2";
 import { Step3 } from "./components/Step3";
 import { Step4 } from "./components/Step4";
+import { Step5 } from "./components/Step5";
 
 function App() {
-  const list = [1, 2, 3, 4, 5, 6];
+  const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   const [selectedIndex, setSelectedIndex] = useState(1);
 
   return (
-    <div>
+    <div className="wrapper">
       {/* 根据list渲染出6个元素，点击元素可以改变selectedIndex的值 */}
       <div
         style={{
@@ -22,8 +23,8 @@ function App() {
         {list.map(item => (
           <div
             style={{
-              width: "100px",
-              height: "100px",
+              width: "50px",
+              height: "50px",
               border: "1px solid gray"
             }}
             onClick={() => setSelectedIndex(item)}
@@ -41,8 +42,8 @@ function App() {
       {selectedIndex === 2 && <Step2 />}
       {selectedIndex === 3 && <Step3 />}
       {selectedIndex === 4 && <Step4 />}
+      {selectedIndex === 5 && <Step5 />}
 
-      {selectedIndex === 5 && <div>TODO:我是第5个组件</div>}
       {selectedIndex === 6 && <div>TODO:我是第6个组件</div>}
     </div>
   );
